f6d32756934f750144d46703581bd94f1bcdd7ca
Emiliano Maccaferri <inbox@emilianomaccaferri.com>
2021-02-23 19:10:01 +0100

    up

--- pages/index.vue
+++ pages/index.vue
@@ -1,75 +1,24 @@
 <template>
-  <div class="container">
-    <div>
-      <Logo />
-      <h1 class="title">
-        nuxt-appunti-web
-      </h1>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--grey"
-        >
-          GitHub
-        </a>
+  <div> 
+    <div id="navbar" class="flexbox justify-between">
+      <span id="logo" class="bright">
+        appunti.me
+      </span>
+      <div id="desktop">
+        <nav class="bright">home</nav>
+        <nav class="bright">materie disponibili</nav>
+        <nav class="bright"><b>segnala un bug!</b></nav>
+        <nav class="bright">login</nav>
       </div>
     </div>
+    <section id="main" class="full flexbox align-center justify-center">
+      <div class="container">
+        <h1 class="main" style="text-align: center">appunti.me</h1>
+        <h2 class="main" style="text-align: center;"> fatto da studenti, per gli studenti </h2>
+        <form class="full-width centered">
+          <input type="text" placeholder="Che cosa stai cercando?" class="fancy">
+        </form>
+      </div>
+    </section>
   </div>
 </template>
-
-<script lang="ts">
-import Vue from 'vue'
-
-export default Vue.extend({})
-</script>
-
-<style>
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-
-.title {
-  font-family:
-    'Quicksand',
-    'Source Sans Pro',
-    -apple-system,
-    BlinkMacSystemFont,
-    'Segoe UI',
-    Roboto,
-    'Helvetica Neue',
-    Arial,
-    sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-}
-</style>
